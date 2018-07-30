23031f409e3ab1c1a867f1c094b829dd6efb7918
ChendoChap <39858639+ChendoChap@users.noreply.github.com>
2018-07-30 22:15:34 +0200

    down

--- include/ksdk.h
+++ include/ksdk.h
@@ -13,7 +13,7 @@
 #include "ksdk_util.h"
 #include "ksdk_bsd.h"
 
-#define __Xfast_syscall 0x1C0
+#define __Xfast_syscall 0x3095D0
 
 #define KFUNC(slide, name, ret, args) \
     extern ret (*name) args
